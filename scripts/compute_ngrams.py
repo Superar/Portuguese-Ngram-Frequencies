a925c192f30fe6a86f0aa08cbec8eadd8d437674
Marcio Lima Inácio <marcio@pop-os.lan>
2024-09-27 14:56:35 +0100

    Do ngram counting with pyspark

--- scripts/compute_ngrams.py
+++ scripts/compute_ngrams.py
@@ -1,44 +1,44 @@
-from collections import Counter
+from operator import add
 from pathlib import Path
 
-import conllu
 import polars as pl
+from pyspark.sql import SparkSession
 
+context = dict()
 
-def get_ngrams(sent, n):
+def get_ngrams(line, n):
+    line = line.strip()
     # Split tokens that are bound by =
-    tokens = list()
-    pos = list()
-    for token in sent:
-        forms = token['form'].split('=') if token['form'] != '=' else token['form']
-        tokens.extend(forms)
-        pos.extend([token['upos']] * len(forms))
-        
+    tokens, tags = line.split('\t')
+    new_tokens, new_tags = list(), list()
+    for token, tag in zip(tokens.split(), tags.split()):
+        forms = token.split('=') if token != '=' else token
+        new_tokens.extend(forms)
+        new_tags.extend([tag] * len(forms))
     # Ignore n-grams that are only punctuation (only _ upos)
-    return [' '.join(tokens[i:i+n])
+    return [' '.join(new_tokens[i:i+n])
             for i in range(len(tokens)-n+1)
-            if any(tag != '_' for tag in pos[i:i+n])]
+            if any(tag != '_' for tag in new_tags[i:i+n])]
 
 
-max_n = 5
-counters = [Counter() for _ in range(max_n)]
+n_max = 5
+num_examples_to_save = [10000, 5000, 3000, 1000, 1000]
 
-# corpus_path = Path('data/brwac.conll')
-corpus_path = Path('data/brwac.conll')
-with corpus_path.open('r', encoding='utf-8') as f:
-    for sent in conllu.parse_incr(f):
-        for i in range(max_n):
-            counters[i].update(get_ngrams(sent, i+1))
+corpus_path = Path('data/brwac_awk.txt')
+spark = (SparkSession.builder.appName('PortugueseNgramFrequency').getOrCreate())
+corpus_rdd = spark.sparkContext.textFile(str(corpus_path))
 
-# Convert counts to polars DataFrames
-dfs = [(pl.DataFrame(counter)
-        .unpivot(variable_name='ngram', value_name='freq')
-        .sort(by='freq', descending=True))
-       for counter in counters]
+for i in range(n_max):
+    ngram_rdd = corpus_rdd.flatMap(lambda lines: get_ngrams(lines, i+1))
+    ngram_pairs = ngram_rdd.map(lambda ngram: (ngram.lower(), 1))
+    ngram_counts = ngram_pairs.reduceByKey(add)
+    sorted_ngram_counts = (ngram_counts.sortBy(lambda x: x[1], ascending=False))
 
-# Save results
-for i in range(max_n):
+    df = pl.DataFrame(sorted_ngram_counts.take(num_examples_to_save[i]),
+                      strict=False, orient='row',
+                      schema=[('ngram', pl.Utf8), ('freq', pl.Int32)])
+
+    # Save the n-gram frequencies to a CSV file
     filepath = Path('results') / f'{i+1}gram.csv'
     filepath.parent.mkdir(exist_ok=True, parents=True)
-    df = dfs[i]
     df.write_csv(filepath)
--- scripts/preprocess_brwac.sh
+++ scripts/preprocess_brwac.sh
@@ -1,2 +1,24 @@
-perl -i -ne 'print unless /^# /' data/brwac.conll
-perl -i -ple 'print "" if $. > 1 && /^1\t/' data/brwac.conll
+# perl -i -ne 'print unless /^# /' data/brwac.conll
+# perl -i -ple 'print "" if $. > 1 && /^1\t/' data/brwac.conll
+
+awk '
+BEGIN { sentence_tokens=""; sentence_tags=""; }
+/^$/ {
+    if (sentence_tokens != "" && sentence_tags != "") {
+        print sentence_tokens "\t" sentence_tags;
+        sentence_tokens="";
+        sentence_tags="";
+    }
+}
+!/^#/ && NF > 0 {
+    token=$2;
+    tag=$4;
+    sentence_tokens = (sentence_tokens == "" ? token : sentence_tokens " " token);
+    sentence_tags = (sentence_tags == "" ? tag : sentence_tags " " tag);
+}
+END {
+    if (sentence_tokens != "" && sentence_tags != "") {
+        print sentence_tokens "\t" sentence_tags;
+    }
+}
+' "data/brwac.conll" > "data/brwac_awk.txt"
